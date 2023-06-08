8143a9899a248e67a63aa49b7f9d469b41ef580b
raphael08 <rsiphael@gmail.com>
2023-06-09 02:12:26 +0300

    .

--- fridgeProject/settings.py
+++ fridgeProject/settings.py
@@ -98,7 +98,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'fridgeProject.wsgi.application'
 
-
+STORAGES = {
+    # ...
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
